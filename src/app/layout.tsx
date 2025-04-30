2054355550f2fd7add05eff695afce4b71e8be3f
sumaiya-rahman-chowdhury <bdfara@1>
2025-04-30 18:41:30 +0600

    added scroll to top button

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import ScrollToTopButton from "@/components/shared/ScrollToTopButton";
 // import Navbar from "@/components/navbar/Navbar";
 
 const geistSans = Geist({
@@ -30,6 +31,7 @@ export default function RootLayout({
       >
         {/* <Navbar /> */}
         {children}
+        <ScrollToTopButton/>
       </body>
     </html>
   );
